1093d71e39843770541a18067b5a3a59e121c3ff
Lira Guedes <liracrg@gmail.com>
2023-12-01 18:19:47 -0300

    feat: req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    raise NotImplementedError
+    try:
+        count = 0
+        for entry, out in permanence_period:
+            if entry <= target_time <= out:
+                count += 1
+        return count
+    except TypeError:
+        return None
