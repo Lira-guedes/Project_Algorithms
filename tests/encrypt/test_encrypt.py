a2c84e3568eeb1763d996ffe081358a3b11a0b50
Lira Guedes <liracrg@gmail.com>
2023-12-01 18:59:11 -0300

    feat: req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message('ABCDEF', 3) == "CBA_FED"
+    assert encrypt_message('ABCDEF', 4) == "FE_DCBA"
+    assert encrypt_message('123123', 9) == "321321"
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message('ABCDEF', 'A')
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(3, 4)
